5e6b17b19b2499f132400baba89b33c8fe35406d
胡晗 <huhan_y@163.com>
2022-08-19 10:05:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# disease-report-server
+## 相关文档
+参考文章：https://juejin.im/post/5d036a78f265da1bce3dcd40
+
+## QuickStart
+
+<!-- add docs here for user -->
+
+see [egg docs][egg] for more detail.
+
+### Development
+
+```bash
+$ npm i
+$ npm run dev
+$ open http://localhost:7001/
+```
+
+### Deploy
+
+```bash
+$ npm start
+$ npm stop
+```
+
+### npm scripts
+
+- Use `npm run lint` to check code style.
+- Use `npm test` to run unit test.
+- Use `npm run autod` to auto detect dependencies upgrade, see [autod](https://www.npmjs.com/package/autod) for more detail.
+
+
+[egg]: https://eggjs.org
---
+++ app/controller/report.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const moment = require('moment');
+
+class ReportController extends Controller {
+  async index() {
+    const { ctx } = this;
+    const { header,body } = ctx.request
+    const url = `${header.origin}/pdf/${body.id}`
+    
+    // 启动pupeteer，加载页面
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    });
+    await page.goto(url, {
+      waitUntil: 'networkidle0'
+    });
+
+    // 生成pdf
+    let pdfFileName = `${body.name}-成绩单-${moment(new Date()).format('YYYYMMDDHHmm')}.pdf`
+    let pdfFilePath = path.join(__dirname, '../../temp/', pdfFileName);
+    await page.pdf({
+      path: pdfFilePath,
+      format: 'A4',
+      scale: 1,
+      printBackground: true,
+      landscape: false,
+      displayHeaderFooter: false
+    });
+    // 关闭浏览器
+    browser.close();
+    // 返回文件路径
+    ctx.status = 200
+    ctx.body ={
+      filePata: `/resource/${pdfFileName}`
+    }
+  }
+}
+
+module.exports = ReportController;
